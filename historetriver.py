36e0577894673122516feee21f1705e0121f208f
Bill Swearingen <hevnsnt@gmail.com>
2016-07-15 15:51:30 -0500

    This file queries the network for details and saves it out to a cvs file.  it is pretty hacky.

--- historetriver.py
+++ historetriver.py
@@ -5,30 +5,30 @@
 ##########################################################
 
 import csv #CSV Library for writing!
-import subprocess 
+import subprocess
 from json import loads
 from datetime import datetime
 
 
 def getoutput(command,argument):
-	p = subprocess.Popen([command, argument], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	out, err = p.communicate()
-	out = loads(out)
-	return out
+        p = subprocess.Popen([command, argument], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        out = loads(out)
+        return out
 
 def writecsf(infoDict):
-	with open('history.csv', 'a') as f: 
-		w = csv.DictWriter(f, infoDict.keys())
-		w.writeheader()
-		w.writerow(infoDict)
+        with open('/home/seckcoin/bin/history.csv', 'a') as f:
+                w = csv.DictWriter(f, infoDict.keys())
+                w.writeheader()
+                w.writerow(infoDict)
 
 def main():
-	miningInfo = getoutput('seckcoind','getmininginfo')
-	generalInfo = getoutput('seckcoind','getinfo')
-	seckcoinInfo = generalInfo.copy() # In order to merge dicts that have same keys, copy to new dict
-	seckcoinInfo.update(miningInfo) # Then update with other dict (this has priority)
-	seckcoinInfo['timestamp'] = datetime.today()
-	writecsf(seckcoinInfo)
+        miningInfo = getoutput('/home/seckcoin/bin/seckcoind','getmininginfo')
+        generalInfo = getoutput('/home/seckcoin/bin/seckcoind','getinfo')
+        seckcoinInfo = generalInfo.copy() # In order to merge dicts that have same keys, copy to new dict
+        seckcoinInfo.update(miningInfo) # Then update with other dict (this has priority)
+        seckcoinInfo['timestamp'] = datetime.today()
+        writecsf(seckcoinInfo)
 
 
 if __name__ == "__main__":
